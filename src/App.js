9a415083c346d12215029feda1d0614e580a8238
julyyu <yuhaocan@sina.com>
2018-02-02 19:22:11 +0800

    急事，先提交了先 。回来继续写

--- src/App.js
+++ src/App.js
@@ -16,9 +16,14 @@ import TodayGank from './component/TodayGank';
 import MachineGank from './component/MachineGank';
 import Header from './component/Header';
 import TimeLine from './page/TimeLine';
-const { Content, Footer } = Layout;
+const { Content, Footer, Sider } = Layout;
 class App extends Component {
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      collapsed: false
+    }
+  }
   componentDidMount = () => {
     this.props.loadGankDates();
   }
@@ -33,25 +38,52 @@ class App extends Component {
         </BackTop> */}
         <BackTop />
         <Layout>
-          <Header />
-          <Content style={{ padding: '0 50px', marginTop: 64 }}>
-            {/* <Breadcrumb style={{ margin: '16px 0' }}>
+          <Sider
+            trigger={null}
+            collapsible
+            collapsed={this.state.collapsed}
+          >
+            <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              style={{ lineHeight: '63px', float: 'left' }}
+            >
+              <Menu.Item key="1"><Link to='/'><span>最新</span></Link ></Menu.Item>
+              <Menu.Item key="2"><Link to='/Android'><span>安卓</span></Link ></Menu.Item>
+              <Menu.Item key="3"><Link to='/iOS'><span>苹果</span></Link ></Menu.Item>
+              <Menu.Item key="4"><Link to='/front'><span>前端</span></Link ></Menu.Item>
+              <Menu.Item key="5"><Link to='/app'><span>App</span></Link ></Menu.Item>
+              <Menu.Item key="6"><Link to='/more'><span>拓展知识</span></Link ></Menu.Item>
+              <Menu.Item key="7"><Link to='/video'><span>休息视频</span></Link ></Menu.Item>
+              <Menu.Item key="8"><Link to='/girls'><span>福利</span></Link ></Menu.Item>
+              <Menu.Item key="9"><Link to='/time'><span>时光机</span></Link ></Menu.Item>
+              {/* <Menu.Item style={{float: 'right'}}>
+                
+             </Menu.Item> */}
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header />
+            <Content style={{ padding: '0 50px', marginTop: 64 }}>
+              {/* <Breadcrumb style={{ margin: '16px 0' }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb> */}
-            <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
-              <Switch>
-                <Route exact path="/" component={TodayGank} />
-                <Route path='/time' component={TimeLine} />
-                <Route path='/timeMachine/:date' component={MachineGank} />
-                <Route path="/:classify" component={GankList} />
-              </Switch>
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            <span>ReactGank ©2017 UI by Ant Design & API by GankIo & Power by React</span>
-          </Footer>
+              <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>
+                <Switch>
+                  <Route exact path="/" component={TodayGank} />
+                  <Route path='/time' component={TimeLine} />
+                  <Route path='/timeMachine/:date' component={MachineGank} />
+                  <Route path="/:classify" component={GankList} />
+                </Switch>
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+              <span>ReactGank ©2017 UI by Ant Design & API by GankIo & Power by React</span>
+            </Footer>
+          </Layout>
         </Layout>
       </Spin >
     );
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,35 +1,16 @@
 import React from 'react'
-import { Layout, Menu, Breadcrumb, Spin,Icon,Button } from 'antd';
+import { Layout, Menu, Breadcrumb, Spin, Icon, Button } from 'antd';
 import { Link } from 'react-router-dom';
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
-export default function GankHeader(){
+export default function GankHeader() {
     return (
         <Header style={{ width: '100%' }}>
             <div className="logo" />
-            <Menu
-              theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={['1']}
-              style={{ lineHeight: '63px', float: 'left' }}
-            >
-              <Menu.Item key="1"><Link to='/'><span>最新</span></Link ></Menu.Item>
-              <Menu.Item key="2"><Link to='/Android'><span>安卓</span></Link ></Menu.Item>
-              <Menu.Item key="3"><Link to='/iOS'><span>苹果</span></Link ></Menu.Item>
-              <Menu.Item key="4"><Link to='/front'><span>前端</span></Link ></Menu.Item>
-              <Menu.Item key="5"><Link to='/app'><span>App</span></Link ></Menu.Item>
-              <Menu.Item key="6"><Link to='/more'><span>拓展知识</span></Link ></Menu.Item>
-              <Menu.Item key="7"><Link to='/video'><span>休息视频</span></Link ></Menu.Item>
-              <Menu.Item key="8"><Link to='/girls'><span>福利</span></Link ></Menu.Item>
-              <Menu.Item key="9"><Link to='/time'><span>时光机</span></Link ></Menu.Item>
-              {/* <Menu.Item style={{float: 'right'}}>
-                
-             </Menu.Item> */}
-             </Menu>
-             <a href='https://github.com/yuhaocan/React-Gank' target="_blank"><Icon type="github" style={{fontSize:'25px',float:'right',lineHeight: '63px', }}/> </a>
+            <a href='https://github.com/yuhaocan/React-Gank' target="_blank"><Icon type="github" style={{ fontSize: '25px', float: 'right', lineHeight: '63px', }} /> </a>
         </Header>
     )
-    function toGitHub(){
-        window.open( 'https://github.com/yuhaocan/React-Gank' );
+    function toGitHub() {
+        window.open('https://github.com/yuhaocan/React-Gank');
     }
 }
